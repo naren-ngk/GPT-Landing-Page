3ddb6f645b2964ce4168dfef79b18d785302e0e8
naren-ngk <narensakthiit@gmail.com>
2023-05-02 16:22:10 +0530

    First Commit

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cta.css';
+
+function CTA() {
+    return (
+        <div>
+            Cta
+        </div>
+    )
+}
+
+export default CTA
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+
+function Navbar() {
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
